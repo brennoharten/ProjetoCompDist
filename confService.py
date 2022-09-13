2d4c0b333588dc81e515151b1eb7aa3acb408fa0
brennoflorencio <brenno.florencio@aec.com.br>
2022-09-13 17:28:32 -0300

    projeto concluido

--- confService.py
+++ confService.py
@@ -37,15 +37,26 @@ def central():
     k = minimo.get(1.0, "end-1c")
     p = confInd.get(1.0, "end-1c")
     dados = confCalc(int(n), int(k), float(p))
+    dadoscomp = confCalc(int(1), int(1), float(p))
     df1 = DataFrame(dados, columns=['maquinas', 'confiabilidade'])
+    df2 = DataFrame(dadoscomp, columns=['maquinas', 'confiabilidade'])
     root = tk.Tk()
+
     figure2 = plt.Figure(figsize=(6, 6), dpi=100)
     ax1 = figure2.add_subplot(111)
     line2 = FigureCanvasTkAgg(figure2, root)
     line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
     df1 = df1[['maquinas', 'confiabilidade']].groupby('maquinas').sum()
     df1.plot(kind='line', legend=True, ax=ax1, color='r', marker='o', fontsize=10)
-    ax1.set_title('maquinas Vs. confiabilidade')
+    ax1.set_title('caso com replicação')
+
+    figure2 = plt.Figure(figsize=(6, 6), dpi=100)
+    ax2 = figure2.add_subplot(111)
+    line2 = FigureCanvasTkAgg(figure2, root)
+    line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    df2 = df2[['maquinas', 'confiabilidade']].groupby('maquinas').sum()
+    df2.plot(kind='line', legend=True, ax=ax2, color='r', marker='o', fontsize=10)
+    ax2.set_title('caso sem replicação')
     
     root.mainloop()
 
